a49cd20ef18caba91a7c04f2ff91afe39e03ff59
Eleonora Hwang <eleonorahwang@gmail.com>
2022-04-13 15:00:49 -0700

    Created copy of env file and changed DATABASE_URL value to database's url. Created new database with tables: tanks, inhabitants, conditions, and images. Required pg package in index.js. Created uploads-middleware

---
+++ database/data.sql
@@ -0,0 +1,5 @@
+insert into "images" ("imageUrl")
+values ('/images/myaquarium.jpg');
+
+insert into "tanks" ("name", "imageId", "size")
+values ('Bikini Bottom', 1, 30);
--- server/index.js
+++ server/index.js
@@ -1,5 +1,7 @@
 require('dotenv/config');
+const pg = require('pg');
 const express = require('express');
+// const ClientError = require('./client-error');
 const errorMiddleware = require('./error-middleware');
 const staticMiddleware = require('./static-middleware');
 
@@ -7,9 +9,22 @@ const app = express();
 
 app.use(staticMiddleware);
 
+const jsonMiddleware = express.json();
+
+app.use(jsonMiddleware);
+
 app.use(errorMiddleware);
 
 app.listen(process.env.PORT, () => {
   // eslint-disable-next-line no-console
   console.log(`express server listening on port ${process.env.PORT}`);
 });
+
+const db = new pg.Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
+});
+
+db();
