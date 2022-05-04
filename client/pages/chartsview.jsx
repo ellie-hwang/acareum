5418dbef45a4b88d79d28dae09ddf872a607aa5d
Eleonora Hwang <eleonorahwang@gmail.com>
2022-05-03 17:44:30 -0700

    Defined handleSubmit() function. If input value is 7, fetch makes a get request for conditions from past 7 days. If input value is 30, fetch makes a get request for conditions from past 30 days.

--- client/pages/chartsview.jsx
+++ client/pages/chartsview.jsx
@@ -7,17 +7,42 @@ export default class ChartsView extends React.Component {
     this.state = {
       tankId: this.props.tankId,
       condition: '',
-      timeSpan: ''
+      timeSpan: '',
+      data: []
     };
-    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
     this.setConditionTimeSpan = this.setConditionTimeSpan.bind(this);
   }
 
-  handleChange(event) {
+  handleSubmit(event) {
     event.preventDefault();
+    if (Number(this.state.timeSpan) === 7) {
+      fetch(`/api/conditions/week/${this.state.tankId}`)
+        .then(res => res.json())
+        .then(data => this.setState({ data }))
+        .catch(error => {
+          console.error('Error:', error);
+        });
+    } else if (Number(this.state.timeSpan) === 30) {
+      fetch(`/api/conditions/month/${this.state.tankId}`)
+        .then(res => res.json())
+        .then(data => this.setState({ data }))
+        .catch(error => {
+          console.error('Error:', error);
+        });
+    }
   }
 
   setConditionTimeSpan(event) {
+    if (event.target.getAttribute('id') === 'timeSpan') {
+      this.setState({
+        timeSpan: event.target.value
+      });
+    } else if (event.target.getAttribute('id') === 'condition') {
+      this.setState({
+        condition: event.target.value
+      });
+    }
   }
 
   render() {
@@ -32,11 +57,11 @@ export default class ChartsView extends React.Component {
           <div className="row">
             <ChartComp tankId={this.state.tankId} condition={this.state.condition} timeSpan={this.state.timeSpan} />
             <div className="col-12 col-sm-6 col-md-6">
-              <form>
+              <form onSubmit={this.handleSubmit}>
                 <div className="mb-3">
                   <label htmlFor="timeSpan" className="form-label">Time Frame</label>
                   <select required className="form-select" id="timeSpan" onChange={this.setConditionTimeSpan}>
-                    <option value="DEFAULT" hidden>Choose time span</option>
+                    <option value="" hidden>Choose time span</option>
                     <option value="7">Past 7 Days</option>
                     <option value="30">Past 30 Days</option>
                   </select>
@@ -44,13 +69,16 @@ export default class ChartsView extends React.Component {
                 <div className="mb-3">
                   <label htmlFor="condition" className="form-label">Condition</label>
                   <select required className="form-select" id="condition" onChange={this.setConditionTimeSpan}>
-                    <option value="DEFAULT" hidden>Choose condition</option>
+                    <option value="" hidden>Choose condition</option>
                     <option value="pH">pH</option>
                     <option value="temperature">temperature</option>
                     <option value="ammonia">ammonia</option>
                     <option value="nitrite">nitrite</option>
                     <option value="nitrate">nitrate</option>
                   </select>
+                </div>
+                <div className="text-end">
+                  <button type="submit">View Chart</button>
                 </div>
               </form>
             </div>
