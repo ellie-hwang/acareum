8fb4420309a9ad582bb17602c84d8bc6869307f2
Eleonora Hwang <eleonorahwang@gmail.com>
2022-05-03 23:30:40 -0700

    Defined ChartComp component. Chart renders on submit with appropriate data points

--- client/pages/chartsview.jsx
+++ client/pages/chartsview.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-// import { Chart } from 'react-google-charts';
+import { Chart } from 'react-google-charts';
 
 export default class ChartsView extends React.Component {
   constructor(props) {
@@ -55,7 +55,7 @@ export default class ChartsView extends React.Component {
             </div>
           </div>
           <div className="row">
-            <ChartComp tankId={this.state.tankId} condition={this.state.condition} timeSpan={this.state.timeSpan} />
+            <ChartComp tankId={this.state.tankId} condition={this.state.condition} timeSpan={this.state.timeSpan} data={this.state.data} />
             <div className="col-12 col-sm-6 col-md-6">
               <form onSubmit={this.handleSubmit}>
                 <div className="mb-3">
@@ -89,16 +89,90 @@ export default class ChartsView extends React.Component {
   }
 }
 
+// function padTwoDigits(number) {
+//   return number.toString().padStart(2, '0');
+// }
+
+// function getMonthDate(date) {
+//   return [
+//     padTwoDigits(date.getMonth() + 1),
+//     padTwoDigits(date.getDate()),
+//     padTwoDigits(date.getFullYear())
+//   ].join('/');
+// }
+
+// function getDate(date) {
+
+// }
+
 function ChartComp(props) {
-  const { condition, timeSpan } = props;
+  const { condition, data } = props;
+  const chartData = [[{ type: 'date', label: 'Date' }, { type: 'number', label: { condition } }]];
+  for (let i = 0; i < data.length; i++) {
+    const dataPoint = [];
+    const date = new Date(data[i].max_dateLogged);
+    const conditionValue = Number(Number(data[i][condition]).toFixed(1));
+    dataPoint.push(date);
+    dataPoint.push(conditionValue);
+    chartData.push(dataPoint);
+  }
+
+  const options = {
+    legend: {
+      position: 'none'
+    },
+    chartArea: {
+      bottom: 60
+    },
+    lineWidth: 3,
+    backgroundColor: {
+      fill: '#000'
+    },
+    colors: ['#5E60CE'],
+    hAxis: {
+      title: 'Date',
+      titleTextStyle: {
+        color: '#FFF',
+        fontSize: 14,
+        bold: true
+      },
+      format: 'MM/dd',
+      textStyle: {
+        color: 'white'
+      }
+    },
+    vAxis: {
+      title: `${condition} Level`,
+      titleTextStyle: {
+        color: '#FFF',
+        fontSize: 14,
+        bold: true
+      },
+      minValue: 0,
+      maxValue: 14,
+      textStyle: {
+        color: 'white'
+      }
+    }
+  };
+
+  const displayImg = data.length === 0 ? '' : 'display-none';
+  const displayChart = data.length !== 0 ? '' : 'display-none';
   return (
     <div className="col-12 col-sm-6 col-md-6">
       <div className="chart-frame mb-3">
-        {
-          (!condition || !timeSpan) &&
-          <img src="images/placeholder-chart.png" alt="chart placeholder image" className="chart-img" />
-        }
+        <img src="images/placeholder-chart.png" alt="chart placeholder image" className={`chart-img ${displayImg}`} />
+        <div className={displayChart}>
+          <Chart
+            chartType="LineChart"
+            width="auto"
+            height="auto"
+            data={chartData}
+            options={options}
+          />
+        </div>
       </div>
     </div>
   );
+
 }
